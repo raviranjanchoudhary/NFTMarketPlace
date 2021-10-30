7ed070edade81895f8770fba3d0ccc2fc46b1696
raviranjanims <raviranjan141287@gmail.com>
2021-10-30 21:39:23 +0800

    ERC721 and ERC20 tokens

---
+++ contracts/ArtifactToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ArtifactToken is ERC721{
+
+    struct Artifact{
+        string name;
+        string createdBy;
+    }
+
+    Artifact[] private artificats;
+
+    event Mint(uint256 artIndex, address owner);
+
+    constructor() ERC721("Artifact Token","ATT"){
+
+    }
+
+    function _mintArtifcat(address owner, string memory name, string memory createdBy) internal returns (uint256){
+
+        Artifact memory artifact = Artifact({
+            name:name,
+            createdBy:createdBy
+        });
+
+        artificats.push(artifact);
+
+        uint256 index = artificats.length-1;
+        emit Mint(index,msg.sender);
+
+        super._mint(owner, index);
+        emit Transfer(address(0), owner, index);
+
+        return index;
+    }
+}
